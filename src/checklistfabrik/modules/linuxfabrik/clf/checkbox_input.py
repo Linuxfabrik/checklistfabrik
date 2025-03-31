365eb480508cc3d3717cc13b55206abba6a3d6ad
Marco Benedetti <marco.benedetti@linuxfabrik.ch>
2025-03-31 15:38:36 +0200

    feat(modules): support checkbox groups

--- src/checklistfabrik/modules/linuxfabrik/clf/checkbox_input.py
+++ src/checklistfabrik/modules/linuxfabrik/clf/checkbox_input.py
@@ -1,32 +1,46 @@
 """
 ChecklistFabrik checkbox_input module
 
-This module renders an HTML checkbox input field.
+This module renders a group of HTML checkbox input fields.
 
 EXAMPLE::
 
     - linuxfabrik.clf.checkbox_input:
-        label: 'Are backups tested for host {{ host }}?'
+        label: 'Steps to reproduce issue {{ issue_id }}?'
+        values:
+            - 'Step 1'
+            - 'Step 2'
+            - 'Step 3'
         required: true
-      fact_name: 'backups_tested'
+      fact_name: 'reproduce_issue'
 """
 
 import jinja2
 
-TEMPLATE_STRING = '''
-<div class="form-group">
-    <label class="form-checkbox">
-        <input name="{{ fact_name }}" type="checkbox"
-            {%- if fact_value %} checked="checked" {%- endif %}
-            {%- if required %} required="required" {%- endif %}/>
-        <i class="form-icon"></i>{{ templated_label }}
-    </label>
-</div>
+TEMPLATE_STRING = '''\
+<fieldset>
+    {% if label %}
+    <label>{{ templated_label }}</label>
+    {% endif %}
+
+    {% for value in templated_values %}
+    <div class="form-group">
+        <label class="form-checkbox">
+            <input name="{{ fact_name }}" type="checkbox" value="{{ value }}"
+                {%- if value == fact_value or (not fact_value is string and value in fact_value) %} checked="checked" {%- endif %}
+                {%- if required %} required="required" {%- endif %}/>
+            <i class="form-icon"></i>{{ value }}
+        </label>
+    </div>
+    {% endfor %}
+</fieldset>
 '''
 
 
 def main(**kwargs):
     templated_label = jinja2.Template(kwargs.get('label', '')).render(**kwargs)
+
+    templated_values = [jinja2.Template(value).render(**kwargs) for value in kwargs.get('values', [])]
 
     return {
         'html': jinja2.Template(
@@ -35,5 +49,6 @@ def main(**kwargs):
             **kwargs,
             fact_value=kwargs.get(kwargs.get('fact_name')),
             templated_label=templated_label,
+            templated_values=templated_values,
         ),
     }

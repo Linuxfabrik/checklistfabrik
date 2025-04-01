d9fc666732810caed7c598c329ea3773f40967e5
Marco Benedetti <marco.benedetti@linuxfabrik.ch>
2025-04-01 14:01:24 +0200

    feat(modules): support multi select and templated select options

--- src/checklistfabrik/modules/linuxfabrik/clf/select_input.py
+++ src/checklistfabrik/modules/linuxfabrik/clf/select_input.py
@@ -22,10 +22,13 @@ TEMPLATE_STRING = '''
 <div class="form-group">
     <label class="form-label" for={{ fact_name }}>{{ templated_label }}</label>
     <select class="form-select" id="{{ fact_name }}" name="{{ fact_name }}"
-        {%- if required %} required="required" {%- endif %}>
+        {%- if required %} required="required" {%- endif %}
+        {%- if multiple %} multiple="multiple" {%- endif %}>
+        {% if not multiple %}
         <option value="">--- Please select ---</option>
-        {% for value in values %}
-        <option {%- if fact_value == value %} selected="selected" {%- endif %}>{{ value }}</option>
+        {% endif %}
+        {% for value in templated_values %}
+        <option {%- if value == fact_value or (not fact_value is string and value in fact_value) %} selected="selected" {%- endif %}>{{ value }}</option>
         {% endfor %}
     </select>
 </div>
@@ -36,13 +39,16 @@ def main(**kwargs):
     fact_name = kwargs['fact_name' if 'fact_name' in kwargs else 'auto_fact_name']
     templated_label = mistune.html(jinja2.Template(kwargs.get('label', '')).render(**kwargs))
 
+    templated_values = [jinja2.Template(value).render(**kwargs) for value in kwargs.get('values', [''])]
+
     return {
         'html': jinja2.Template(
             TEMPLATE_STRING,
         ).render(
             **kwargs,
-            fact_value=kwargs.get(fact_name),
+            fact_value=kwargs.get(fact_name, []),
             templated_label=templated_label,
+            templated_values=templated_values,
         ),
         'fact_name': fact_name,
     }
